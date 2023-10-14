7d4f87c4aa189bd1b56e132722e18aa0bebda367
Ayoush <ayoushchourasia1418@gmail.com>
2023-10-14 09:00:31 +0530

    Initial project setup

---
+++ lib/cmdissues.ex
@@ -0,0 +1,18 @@
+defmodule Cmdissues do
+  @moduledoc """
+  Documentation for `Cmdissues`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cmdissues.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cmdissues_test.exs
@@ -0,0 +1,8 @@
+defmodule CmdissuesTest do
+  use ExUnit.Case
+  doctest Cmdissues
+
+  test "greets the world" do
+    assert Cmdissues.hello() == :world
+  end
+end
